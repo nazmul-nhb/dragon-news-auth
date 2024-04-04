a83ce0a3ee0b117d72482261daf48c0bde218758
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-04-04 15:39:29 +0600

    started working on left nav

--- src/pages/shared/LeftNav.jsx
+++ src/pages/shared/LeftNav.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left Navigation</h2>
+        <div className="">
+            <h2 className="text-2xl mb-5">All Category</h2>
+            {
+                categories.map(category => <NavLink
+                    to={`/category/${category?.id}`}
+                    className="flex flex-col gap-2 px-8 py-4 font-semibold"
+                    key={category?.id}>{category?.name}</NavLink>)
+            }
         </div>
     );
 };
